e36366d93d6afd90d918b8c42e16c95481c0ae18
Saransh <saranshgautam07@gmail.com>
2023-06-10 10:33:48 +0530

    optimal

---
+++ subarrSumK-2.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+class{
+    public:
+    int subarraysWithSumK(vector<int> &nums,int k){
+        unordered_map<int,int>mpp;
+        mpp[0]=1;
+        int preSum=0,count=0;
+        for(int i=0;i<nums.size();i++){
+            preSum+=nums[i];
+            int remove=preSum-k;
+            count+=mpp[remove];
+            mpp[preSum]+=1;
+
+
+        }
+        return count;
+
+    }
+
+};
