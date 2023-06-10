b39e5115395e9c9e3d8e8d655876f138ab49ae22
Saransh <saranshgautam07@gmail.com>
2023-06-10 09:33:12 +0530

    code

---
+++ subarraySumK.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+    int n = nums.size(); // size of the given array.
+    int cnt = 0; // Number of subarrays:
+
+    for (int i = 0 ; i < n; i++) { // starting index i
+        int sum = 0;
+        for (int j = i; j < n; j++) { // ending index j
+            // calculate the sum of subarray [i...j]
+            // sum of [i..j-1] + nums[j]
+            sum +=nums[j];
+
+            // Increase the count if sum == k:
+            if (sum == k)
+                cnt++;
+        }
+    }
+    return cnt;
+    }
+};
