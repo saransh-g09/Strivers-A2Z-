494407a73ee72a5126ef0f24e4f9b5d5aefb105d
Saransh <saranshgautam07@gmail.com>
2023-06-13 23:41:00 +0530

    code

---
+++ MajorityElement2.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int n = nums.size(); //size of the array
+    vector<int> ls; // list of answers
+
+    //declaring a map:
+    map<int, int> mpp;
+
+    // least occurrence of the majority element:
+    int mini = int(n / 3) + 1;
+
+    //storing the elements with its occurnce:
+    for (int i = 0; i < n; i++) {
+        mpp[nums[i]]++;
+
+        //checking if nums[i] is
+        // the majority element:
+        if (mpp[nums[i]] == mini) {
+            ls.push_back(nums[i]);
+        }
+        if (ls.size() == 2) break;
+    }
+
+    return ls;
+    }
+};
