e2b0886c6ee2c7411e0ac13b47bef39034b06fbc
Saransh <saranshgautam07@gmail.com>
2023-06-24 22:13:16 +0530

    code

---
+++ BS/BinarySearch.cpp
@@ -0,0 +1,21 @@
+/*finding target value in sorted array using binary search and doing BS and using recursive approach*/
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int bs(vector<int>& nums,int low,int high,int target){
+        if(low>high){
+            return -1;
+        }
+        int mid=low + (high-low)/2;
+        if(nums[mid]==target) return mid;
+        else if(target>nums[mid]){
+            return bs(nums,mid+1,high,target);
+        }
+        return bs(nums,low,mid-1,target);
+    }
+    int search(vector<int>& nums, int target) {
+        return bs(nums,0,nums.size()-1,target);
+        
+    }
+};
